fe3fae3ca2ea3879f1d21af559a1738e5d3eea18
Rub21 <rub2106@gmail.com>
2019-08-20 16:57:23 -0500

    filter update

--- mx/mx.sh
+++ mx/mx.sh
@@ -3,8 +3,8 @@ path=data
 mkdir -p data
 
 docker run --rm -v ${PWD}:/mnt/data developmentseed/geokit:latest geojson2poly total-area.geojson  $path/bounduary.poly
-http://download.geofabrik.de/north-america.html
-wget http://download.geofabrik.de/north-america/mexico-latest.osm.pbf -O $path/mexico.pbf
+# http://download.geofabrik.de/north-america.html
+# wget http://download.geofabrik.de/north-america/mexico-latest.osm.pbf -O $path/mexico.pbf
 
 docker run --rm -v ${PWD}:/mnt/data developmentseed/geokit:latest osmconvert \
 $path/mexico.pbf -B=$path/bounduary.poly -o=$path/juchitan.pbf
@@ -18,8 +18,6 @@ $path/juchitan.osm --keep="building" > $path/juchitan-building.osm
 docker run --rm -v ${PWD}:/mnt/data developmentseed/geokit:latest geokit fc2frows \
 div.geojson > $path/row.json
 
-# docker run --rm -v ${PWD}:/mnt/data developmentseed/geokit:latest geokit osm2new \
-# $path/juchitan-building.osm $path/juchitan-building-new.osm
 
 i=0
 while IFS= read -r line; do
@@ -27,6 +25,9 @@ while IFS= read -r line; do
     docker run --rm -v ${PWD}:/mnt/data developmentseed/geokit:latest geojson2poly $path/feature.json  $path/feature.poly
     docker run --rm -v ${PWD}:/mnt/data developmentseed/geokit:latest osmconvert \
         $path/juchitan-building.osm -B=$path/feature.poly -o=$path/juchitan-building-$i.osm
+    docker run --rm -v ${PWD}:/mnt/data developmentseed/geokit:latest geokit osm2new \
+        $path/juchitan-building-$i.osm $path/juchitan-building-$i-new.osm
+    rm $path/juchitan-building-$i.osm
     i=$(($i + 1))
 done < $path/row.json
 
